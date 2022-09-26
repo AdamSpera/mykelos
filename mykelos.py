6a0a7c6f5152091e3ae887ea10192dc7179184f5
AdamSpera <adamspera@hotmail.com>
2022-09-25 20:47:07 -0400

    Add file structure

--- mykelos.py
+++ mykelos.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template', static_folder='static')
 
 @app.route("/")
 def sendStatic():
