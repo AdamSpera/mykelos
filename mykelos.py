1dc1de09448b82a071d28f8c8539dda046c0093d
AdamSpera <adamspera@hotmail.com>
2022-09-26 09:21:05 -0400

    Add basic web interface generation

--- mykelos.py
+++ mykelos.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask import request
 from flask import render_template
 from netmiko import ConnectHandler
 import inquirer
 import json
 import sys
 
+# Create and initialized the Flask app
+app = Flask(__name__, template_folder='template', static_folder='static')
+
+# Connect to device via SSH
 cisco_881 = {
     'device_type': 'cisco_ios',
     'host':   '192.168.0.1',
@@ -12,26 +17,32 @@ cisco_881 = {
     'password': 'password',
     "secret": 'password',
 }
+# Initiate SSH connection
 net_connect = ConnectHandler(**cisco_881)
+# Enter enable mode
 net_connect.enable()
-
-print("Connection established!\n")
-
-# Get the hostname of the device
-# deviceHostname = net_connect.send_command("show running-config", use_textfsm=True).split('!')[2].split('hostname')[1].strip()
-# print(deviceHostname)
-
-# Get the interfaces of the device
-# output = net_connect.send_command("show ip interface brief", use_textfsm=True)
-# allInterfaces = []
-# for i in output:
-#     allInterfaces.append(i["intf"])
-# print(allInterfaces)
-
-# Flask options below \/ ----------------------------------------------
-
-app = Flask(__name__, template_folder='template', static_folder='static')
+# Confirmation message
+print(" # Connection established!\n")
 
 @app.route("/")
 def sendStatic():
+    # Return the index.html file
     return render_template('index.html')
+
+@app.route('/getHostname', methods=['GET'])
+def getHostname():
+    # Return the hostname of the device
+    return net_connect.send_command("show running-config", use_textfsm=True).split('!')[2].split('hostname')[1].strip()
+
+@app.route('/getInterfaces', methods=['GET'])
+def getInterfaces():
+
+    return net_connect.send_command("show ip interface brief", use_textfsm=True)
+
+    # allInterfaces= []
+    # # Get all interfaces from device
+    # for i in net_connect.send_command("show ip interface brief", use_textfsm=True):
+    #     allInterfaces.append(i["intf"])
+    # # Return all interfaces
+    # return allInterfaces
+
--- static/index.js
+++ static/index.js
@@ -1,1 +1,30 @@
-console.log('Test')
+var title = document.getElementById('title');
+var display = document.getElementById('display');
+
+fetch('/getHostname', { method: 'GET' })
+  .then(response => response.text())
+  .then(text => {
+    title.innerText = text;
+    console.log(text)
+  })
+
+fetch('/getInterfaces', { method: 'GET' })
+  .then(response => response.text())
+  .then(text => {
+    console.log(text)
+    console.log(JSON.parse(text))
+
+    var interfaces = JSON.parse(text);
+    for (let i = 0; i < interfaces.length; i++) {
+      if (!interfaces[i].intf.includes('lan')) {
+        let btn = document.createElement("button");
+        btn.innerText = interfaces[i].intf;
+        btn.onclick = function () {
+          console.log(`${interfaces[i]}: Button is clicked`);
+          display.innerText = `IP Address: ${interfaces[i].ipaddr}\nStatus: ${interfaces[i].status} `;
+        };
+        document.body.appendChild(btn);
+      }
+    }
+
+  })
