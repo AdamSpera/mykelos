213a660fb2f945e10dc4c64721fc672de8dacb29
AdamSpera <adamspera@hotmail.com>
2022-09-25 22:51:32 -0400

    Add netmiko & ntc -templates imports

--- mykelos.py
+++ mykelos.py
@@ -1,8 +1,23 @@
 from flask import Flask
 from flask import render_template
+from netmiko import ConnectHandler
+import inquirer
+import json
+import sys
 
-app = Flask(__name__, template_folder='template', static_folder='static')
+cisco_881 = {
+    'device_type': 'cisco_ios',
+    'host':   '172.168.0.1',
+    'username': 'cisco',
+    'password': 'password',
+    "secret": 'password',
+}
+net_connect = ConnectHandler(**cisco_881)
+net_connect.enable()
+print("Connection established!\n")
 
-@app.route("/")
-def sendStatic():
-    return render_template('index.html')
+# app = Flask(__name__, template_folder='template', static_folder='static')
+
+# @app.route("/")
+# def sendStatic():
+#     return render_template('index.html')
