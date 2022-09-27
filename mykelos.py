3df854608a95b51fc2dfe0c6d1b55ad52a2b919e
AdamSpera <adamspera@hotmail.com>
2022-09-26 20:53:45 -0400

    Add connect pannel and style framework

--- mykelos.py
+++ mykelos.py
@@ -6,35 +6,67 @@ import inquirer
 import json
 import sys
 
+def sendCommand(host, username, gPassword, sPassword, command):
+    # Connect to device via SSH
+    cisco_881 = {
+        'device_type': 'cisco_ios',
+        'host':   host,
+        'username': username,
+        'password': gPassword,
+        "secret": sPassword,
+    }
+    # Initiate SSH connection
+    net_connect = ConnectHandler(**cisco_881)
+    # Enter enable mode
+    net_connect.enable()
+    # Confirmation message
+    print(" # Connection established!\n")
+    # Save result locally
+    sendBack = net_connect.send_command(command, use_textfsm=True)
+    # Disconnect from device
+    net_connect.disconnect()
+    # Send passed command
+    return sendBack
+
 # Create and initialized the Flask app
 app = Flask(__name__, template_folder='template', static_folder='static')
-
-# Connect to device via SSH
-cisco_881 = {
-    'device_type': 'cisco_ios',
-    'host':   '192.168.0.1',
-    'username': 'cisco',
-    'password': 'password',
-    "secret": 'password',
-}
-# Initiate SSH connection
-net_connect = ConnectHandler(**cisco_881)
-# Enter enable mode
-net_connect.enable()
-# Confirmation message
-print(" # Connection established!\n")
 
 @app.route("/")
 def sendStatic():
     # Return the index.html file
     return render_template('index.html')
 
-@app.route('/getHostname', methods=['GET'])
+@app.route('/getHostname', methods=['POST'])
 def getHostname():
+    passedData = json.loads(request.data.decode("utf-8"))
     # Return the hostname of the device
-    return net_connect.send_command("show running-config", use_textfsm=True).split('!')[2].split('hostname')[1].strip()
+    # return net_connect.send_command("show running-config", use_textfsm=True).split('!')[2].split('hostname')[1].strip()
+    return sendCommand(passedData['hostIP'], passedData['username'], passedData['generalPassword'], passedData['secretPassword'], "show running-config").split('!')[2].split('hostname')[1].strip()
 
 @app.route('/getInterfaces', methods=['GET'])
 def getInterfaces():
     # Return the interface brief of the device
     return net_connect.send_command("show ip interface brief", use_textfsm=True)
+
+@app.route('/initConnection', methods=['POST'])
+def initConnection():
+    passedData = json.loads(request.data.decode("utf-8"))
+
+    # Connect to device via SSH
+    cisco_881 = {
+        'device_type': 'cisco_ios',
+        'host':   passedData['hostIP'],
+        'username': passedData['username'],
+        'password': passedData['generalPassword'],
+        "secret": passedData['secretPassword'],
+    }
+    # Initiate SSH connection
+    net_connect = ConnectHandler(**cisco_881)
+    # Enter enable mode
+    net_connect.enable()
+    # Confirmation message
+    print(" # Connection established!\n")
+    # Disconnect from device
+    net_connect.disconnect()
+
+    return 'success'
--- static/index.js
+++ static/index.js
@@ -1,30 +1,41 @@
-var title = document.getElementById('title');
-var display = document.getElementById('display');
+var user = document.getElementById('user');
+var hostField = document.getElementById('hostField');
+var username = document.getElementById('username');
+var generalPassword = document.getElementById('generalPassword');
+var secretPassword = document.getElementById('secretPassword');
+var authenticate = document.getElementById('authenticate');
+var connectionText = document.getElementById('connectionText');
+var loadingAnimation = document.getElementById('loadingAnimation');
 
-fetch('/getHostname', { method: 'GET' })
-  .then(response => response.text())
-  .then(text => {
-    title.innerText = text;
-    console.log(text)
-  })
+var hostname = document.getElementById('hostname');
 
-fetch('/getInterfaces', { method: 'GET' })
-  .then(response => response.text())
-  .then(text => {
-    console.log(text)
-    console.log(JSON.parse(text))
+function getHostname() {
+  fetch('/getHostname', { method: 'POST', body: JSON.stringify({ hostIP: hostField.value, username: username.value, generalPassword: generalPassword.value, secretPassword: secretPassword.value }) })
+    .then(response => response.text())
+    .then(text => {
+      hostname.innerText = text;
+    })
+}
 
-    var interfaces = JSON.parse(text);
-    for (let i = 0; i < interfaces.length; i++) {
-      if (!interfaces[i].intf.includes('lan')) {
-        let btn = document.createElement("button");
-        btn.innerText = interfaces[i].intf;
-        btn.onclick = function () { 
-          console.log(`${interfaces[i]}: Button is clicked`);
-          display.innerText = `IP Address: ${interfaces[i].ipaddr}\nStatus: ${interfaces[i].status} `;
-        };
-        document.body.appendChild(btn);
+authenticate.addEventListener('click', function () {
+  loadingAnimation.style.display = 'block';
+
+  fetch('/initConnection', { method: 'POST', body: JSON.stringify({ hostIP: hostField.value, username: username.value, generalPassword: generalPassword.value, secretPassword: secretPassword.value }) })
+    .then(response => response.text())
+    .then(text => {
+      console.log(text)
+      if (text === 'success') {
+        connectionText.innerText = 'Connection Active';
+        connectionText.style.color = '#20ab4e';
+        loadingAnimation.style.display = 'none';
+        user.innerText = username.value;
+        getHostname();
+      } else {
+        connectionText.innerText = 'Connection Failed';
+        connectionText.style.color = 'rgb(184, 0, 0)';
+        loadingAnimation.style.display = 'none';
       }
-    }
 
-  })
+    })
+
+});
