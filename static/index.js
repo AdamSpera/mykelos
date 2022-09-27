5b16fab47edfe46c3b602e31ccae68f2b8b5a49b
AdamSpera <adamspera@hotmail.com>
2022-09-27 11:01:20 -0400

    Update page design and layout

--- static/index.js
+++ static/index.js
@@ -8,6 +8,18 @@ var connectionText = document.getElementById('connectionText');
 var loadingAnimation = document.getElementById('loadingAnimation');
 
 var hostname = document.getElementById('hostname');
+var interfaces = {
+  'port1': document.getElementById('port1'),
+  'port2': document.getElementById('port2'),
+  'port3': document.getElementById('port3'),
+  'port4': document.getElementById('port4'),
+  'port5': document.getElementById('port5'),
+  'port6': document.getElementById('port6'),
+  'port7': document.getElementById('port7'),
+  'port8': document.getElementById('port8'),
+  'port9': document.getElementById('port9'),
+  'port10': document.getElementById('port10'),
+};
 
 function getHostname() {
   fetch('/getHostname', { method: 'POST', body: JSON.stringify({ hostIP: hostField.value, username: username.value, generalPassword: generalPassword.value, secretPassword: secretPassword.value }) })
