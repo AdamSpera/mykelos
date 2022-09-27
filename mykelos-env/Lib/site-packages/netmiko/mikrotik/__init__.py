9c647030ae1aa160e60f63334cd3ce99d5e6d14f
Adam Spera <64236429+AdamSpera@users.noreply.github.com>
2022-09-27 15:27:56 -0400

    Delete mykelos-env directory

--- mykelos-env/Lib/site-packages/netmiko/mikrotik/__init__.py
+++
@@ -1,5 +0,0 @@
-from netmiko.mikrotik.mikrotik_ssh import MikrotikRouterOsSSH
-from netmiko.mikrotik.mikrotik_ssh import MikrotikSwitchOsSSH
-from netmiko.mikrotik.mikrotik_ssh import MikrotikRouterOsFileTransfer
-
-__all__ = ["MikrotikRouterOsSSH", "MikrotikSwitchOsSSH", "MikrotikRouterOsFileTransfer"]
--- mykelos-env/Lib/site-packages/netmiko/zyxel/__init__.py
+++
@@ -1,4 +0,0 @@
-from netmiko.zyxel.zyxel_ssh import ZyxelSSH
-
-
-__all__ = ["ZyxelSSH"]
